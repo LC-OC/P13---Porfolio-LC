fb04bcaeeb0cd6da8e7fb7ec3e246b986ce20048
Louise Chartier <louise-chartier@outlook.fr>
2024-05-12 23:06:29 +0200

    mobile version

--- wp-content/themes/lc-oc/header.php
+++ wp-content/themes/lc-oc/header.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
     <title>Portfolio LC</title>
     <?php wp_head(); ?>
 </head>
--- wp-content/themes/lc-oc/template-parts/test.php
+++ wp-content/themes/lc-oc/template-parts/test.php
@@ -2,6 +2,7 @@
     <div class="img_project">
         <img src="<?php the_post_thumbnail_url() ?>" alt="Projects <?php the_title() ?> ">
     </div>      
+    <a href="https://portfolio-lc.000webhostapp.com/projets/photographe-event/"></a>
     <a class="link_project" href="<?php echo get_permalink( $post->ID ); ?>">
         <h3><?php the_title() ?></h3>
     </a>      
